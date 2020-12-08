5777512d64d96cd149327064947ad99837b253e1
abhijay-pradhan <73331596+abhijay-pradhan@users.noreply.github.com>
2020-12-08 16:12:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
-function submit() {
+function getresult() {
 	var x = 0;
   var checkBox = document.getElementById("c1");
   var text = document.getElementById("text");
-  if (checkBox.checked == true){
+  if (c1.checked == true){
 	  x = x+1;
   }
-  var radio = document.getElementById("c2");
-  var text = document.getElementById("text");
-  if (radio.checked == true){
-	  x = x+1;
- }
-	var athree = document.getElementById("c3");
-	var text = document.getElementById("text")
-    if (athree.checked == true){
-	  x = x+1;
-  }
-		var afour = document.getElementById("c4");
-	var text = document.getElementById("text")
-    if (afour.checked == true){
-	  x = x+1;
-  }
-		var afive = document.getElementById("c5");
-	var text = document.getElementById("text")
-    if (afive.checked == true){
-	  x = x+1;
-  }
+	var c2 = document.getElementById("c2");
+	if (c2.checked == true){
+		x = x+1;
+	}
+	var c3 = document.getElementById("c3");
+	if (c3.checked == true){
+		x = x+1;
+	}
+	var c4 = document.getElementById("c4");
+	if (c4.checked == true){
+		x = x+1;
+	}
+	var c5 = document.getElementById("c5");
+	if (c5.checked == true){
+		x = x+1;
+	}
 	document.getElementById("result").innerHTML = x + "/5";
 	document.getElementById("resultpercent").innerHTML = x / 5 *100 +" %";
-	if (x == 5){
-	document.getElementById("medal").src = "https://img.pngio.com/gold-medal-gold-medal-png-406_500.png";
-	}
-	document.getElementById("time").innerHTML ="End Time:" + Date();
-	document.getElementById("c1text").innerHTML = "206".fontcolor("green");
-	document.getElementById("c2text").innerHTML = "12".fontcolor("green");
-	document.getElementById("c3text").innerHTML = "Cranium".fontcolor("green");
-	document.getElementById("c4text").innerHTML = "Knee".fontcolor("green");
-	document.getElementById("c5text").innerHTML = "Spinal Cord".fontcolor("green");
-	}
+	document.getElementById("c1text").innerHTML = "3. 206".fontcolor("green");
+	document.getElementById("c2text").innerHTML = "3. 24".fontcolor("green");
+	document.getElementById("c3text").innerHTML = "4. Cranium".fontcolor("green");
+	document.getElementById("c4text").innerHTML = "1. Knee".fontcolor("green");
+	document.getElementById("c5text").innerHTML = "4. Spinal Cord".fontcolor("green");
+}
